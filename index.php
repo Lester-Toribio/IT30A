c2abcc4ac7db9fc7f6b5ce7b9a9c7984369ed206
Lester-Toribio <toribiotitit18@gmail.com>
2026-09-17 10:14:47 +0800

    added student display table on index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 //Database Connection
 $host = 'localhost';
-$db = 'it30a_lab_db';
+$db = 'it30b_lab_db';
 $user = 'root';
 $pass = '';
 $charset = 'utf8mb4';
@@ -66,6 +66,47 @@ if($section==='students'){
     <hr>
     <?php if($section === 'students'): ?>
         <h1>Students</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Course</th>
+                    <th>Created at</th>
+                    <th>Actions</th>
+                </tr>
+            <thead>
+            <tbody>
+                <?php foreach($students as $student): ?>
+                    <tr>
+                        <td>
+                            <?=htmlspecialchars($student['student_id']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_first_name']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_last_name']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_course']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_created_at']) ?>
+                        </td>
+                         <td>
+                            <a>Edit</a>
+                            |
+                            <a>Delete</a>
+                        </td>
+                    </tr>
+                <?php endforeach?>
+            </tbody>
+
+        </table>
+
+
 
     <?php endif;?>
 
